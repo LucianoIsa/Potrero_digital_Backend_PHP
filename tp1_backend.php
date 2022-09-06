e9b55294add693dcc8474253af99929a011fd98d
LUCIANO ISA <lucianoisabb@hotmail.com>
2022-09-06 11:10:29 -0300

    First task php

---
+++ tp1_backend.php
@@ -0,0 +1,38 @@
+<?php
+
+//ejercicio 1 
+
+print  'HOLA MUNDO';
+
+//ejercicio 2
+$saludo = "Hola Mundo";
+echo ($saludo);
+
+//ejercicio 3 
+$a = 4;
+$b = 2;
+$suma = $a + $b;
+$resta = $a - $b;
+$multiplicacion = $a * $b;
+$division = $a / $b;
+$resto = $a % $b;
+print "<p>Suma </p>: $suma</p>";
+print "<p>Resta </p>: $resta</p>";
+print "<p>Multiplicacion </p>: $multiplicaion</p>";
+print "<p>division </p>: $division</p>";
+print "<p>Resto </p>: $resto</p>";
+
+//ejercicio 4
+$temperatura = (20 * (9 / 5) + 32);
+print "$temperatura.ºF\n";
+
+//ejercicio 5
+//a)
+
+$perimetro = (2 * 18) + (2 * 12);
+$area = 18 * 12;
+
+//b)
+
+$perimetro = ((pi() * 2) * 30);
+$area = (pi() * (30 ** 2));
